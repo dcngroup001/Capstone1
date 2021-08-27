6157e0fce2327e90830f6d089a156493fdebc12a
People Grow Together <dcngroup001@gmail.com>
2021-08-27 11:01:09 +1000

    hkpato new commit

--- test.py
+++ test.py
@@ -2,3 +2,5 @@ print("This is a test script")
 print("It's not working")
 print("This is a test from the new jenkins server I created online.....")
 print("Last test for tonight")
+
+print("Kaim kailai enegne")
