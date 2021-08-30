f58334445d8b37d0660c69633ac976ea134b116f
tmou2021 <82550256+tmou2021@users.noreply.github.com>
2021-08-30 13:05:44 +1000

    talu 2

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-kjdkjwdkkjhfkwjdhfkwjdfhprint("This is a test script")
+print("This is a test script")
 print("It's not working")
 print("This is a test from the new jenkins server I created online.....")
 print("Last test for tonight")
@@ -9,4 +9,4 @@ print("Kaim kailai enegne")
 print("Presentation Day")
 print("hello")
 
-print(--0000)
+
